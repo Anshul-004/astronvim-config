c0ae332fc5813698b2fb3d29cd739556fe25652f
Anshul Singh <anshulsingh024@yahoo.com>
2025-08-23 21:07:55 +0530

    Snippets Works

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,6 +1,7 @@
 return {
   "L3MON4D3/LuaSnip",
   opts = function(_, opts)
+    require("luasnip.loaders.from_vscode").lazy_load()
     require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
   end,
 }
